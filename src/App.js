dc0306aa24da561e9352294a0024e8477775e748
Krishna <krishnabiswakarmasc@gmail.com>
2022-07-03 13:44:46 +0530

    Looped over the cards

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import { Box } from '@mui/material';
-import './App.css';
 import MediaCard from './Components/Cards/MediaCard';
 
 function App() {
   return (
     <Box sx={{ display: 'flex', flexDirection: 'row', gap: '1rem', flexWrap: 'wrap' }}>
-      <MediaCard />
-      <MediaCard />
-      <MediaCard />
-    </Box >
+      {[1, 2, 3, 4, 5].map(card => (
+        <MediaCard key={card} />
+      ))}
+    </Box>
   );
 }
 
